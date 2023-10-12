0185a90b052b767b1697dd36bab149e855e3abea
Shoko Okamoto <mffnoissm23@gmail.com>
2023-10-12 17:37:53 +0900

    💄 Logo追加

---
+++ src/components/Logo.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Logo() {
+  return (
+    <h1 className="font-mono font-black text-transparent text-2xl tracking-wider bg-gradient-to-r from-red-500 to-orange-300 bg-clip-text ">
+      MOVIE APP
+    </h1>
+  );
+}
+
+export default Logo;
