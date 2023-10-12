443f961ba62d7c951d4f69cf24c3459a3d400223
Shoko Okamoto <mffnoissm23@gmail.com>
2023-10-12 17:34:21 +0900

    ✨ Sidebar作成

---
+++ src/components/Sidebar/index.jsx
@@ -0,0 +1,58 @@
+import Logo from "@/components/Logo";
+import { useGenres } from "@/hooks/useFetchData";
+import Link from "next/link";
+import React from "react";
+import { AiOutlineHome } from "@react-icons/all-files/ai/AiOutlineHome";
+import { BiCameraMovie } from "@react-icons/all-files/bi/BiCameraMovie";
+import { IconContext } from "@react-icons/all-files";
+
+function Sidebar() {
+  const { data: genreList, error, isLoading } = useGenres();
+
+  return (
+    <div className="w-[15%] min-w-[200px] shrink-0 fixed top-0 left-0 pl-6 pr-4 py-6">
+      <Logo />
+
+      <section className="mt-8">
+        <div className="flex items-center gap-2">
+          <IconContext.Provider value={{ className: "text-xl" }}>
+            <AiOutlineHome />
+          </IconContext.Provider>
+          <Link href="/" className="text-base font-bold">
+            ホーム
+          </Link>
+        </div>
+      </section>
+
+      <section className="mt-8">
+        <div className="flex items-center gap-2">
+          <IconContext.Provider value={{ className: "text-xl" }}>
+            <BiCameraMovie />
+          </IconContext.Provider>
+          <span className="text-base font-bold">ジャンル</span>
+        </div>
+        <ul className="grid gap-1 pl-4 mt-2">
+          {genreList?.genres.map((genre) => {
+            return (
+              <li
+                key={genre.id}
+                className="before:content-[''] before:bg-gray-600 before:w-3 before:h-[1px] before:block flex items-center text-gray-400 gap-2 hover:text-white cursor-pointer"
+              >
+                <Link
+                  href={{
+                    pathname: `/genre/${genre.id}`,
+                    query: { name: genre.name },
+                  }}
+                >
+                  {genre.name}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </section>
+    </div>
+  );
+}
+
+export default Sidebar;
