4a959da97e32854a9acddf0d60b1848348702281
Shoko Okamoto <mffnoissm23@gmail.com>
2023-10-17 18:47:23 +0900

    💄 modalコンテンツ量が多いときの見た目調整

--- src/components/ModalComponent.jsx
+++ src/components/ModalComponent.jsx
@@ -17,11 +17,11 @@ export const ModalComponent = (props) => {
   return (
     <dialog className="block z-[1] bg-black/50 w-full h-screen fixed top-0 left-0">
       <div className="bg-slate-900 w-3/4 m-auto relative top-2/4 translate-y-[-50%] rounded-lg overflow-hidden">
-        <div className="flex">
+        <div className="flex max-h-[80vh]">
           <div className="flex-shrink-0 flex-grow-0 basis-2/5 overflow-hidden">
             <img src={`${TMDB_IMG_URL}${activeMovie.poster_path}`} alt="" />
           </div>
-          <div className="text-white pt-16 p-9 basis-3/5 max-w-[60%]">
+          <div className="text-white pt-16 p-9 basis-3/5 max-w-[60%] overflow-y-auto">
             <p className="text-4xl font-bold">{activeMovie.title}</p>
             <div className="flex gap-2">
               {results?.map((genre) => {
