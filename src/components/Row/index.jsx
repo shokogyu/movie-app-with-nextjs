adf5f828c2cd09e59943771531d13dcdcfa39607
Shoko Okamoto <mffnoissm23@gmail.com>
2023-10-17 17:35:11 +0900

    ⚡️ TOPページのカルーセルデータをISR化

--- src/components/Row/index.jsx
+++ src/components/Row/index.jsx
@@ -1,14 +1,10 @@
 import { EmblaCarousel } from "@/components/EmblaCarousel";
-import { useFetchData } from "@/hooks/useFetchData";
-import { TMDB_API_URL } from "@/utils/const";
 
 export const Row = (props) => {
-  const { data } = useFetchData(props.url ? props.url : null);
-
   return (
     <div className="mb-8">
       <h2 className="text-white text-xl font-bold pl-3">{props.title}</h2>
-      <EmblaCarousel movies={data?.results} />
+      <EmblaCarousel movies={props.data?.results} />
     </div>
   );
 };
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,19 +2,42 @@ import { FirstViewMovie } from "@/components/FirstViewMovie";
 import { Inter } from "next/font/google";
 import { Row } from "@/components/Row";
 import { MyList } from "@/components/MyList";
-import { TMDB_API_URL } from "@/utils/const";
+import { TMDB_API_KEY, TMDB_API_URL } from "@/utils/const";
 import Layout from "@/components/Layout";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+// ISR対応
+export const getStaticProps = async () => {
+  const res = await fetch(`${TMDB_API_URL}/movie/popular?api_key=${TMDB_API_KEY}&language=ja-JA`);
+  const popularData = await res.json();
+
+  const res2 = await fetch(
+    `${TMDB_API_URL}/movie/top_rated?api_key=${TMDB_API_KEY}&language=ja-JA`
+  );
+  const topRatedData = await res2.json();
+
+  const res3 = await fetch(`${TMDB_API_URL}/movie/upcoming?api_key=${TMDB_API_KEY}&language=ja-JA`);
+  const upcomingData = await res3.json();
+
+  return {
+    props: {
+      popularData,
+      topRatedData,
+      upcomingData,
+    },
+    revalidate: 3600, // 1 hour
+  };
+};
+
+export default function Home(props) {
   return (
     <Layout>
       <FirstViewMovie />
       <MyList />
-      <Row title="人気の映画" url={`${TMDB_API_URL}/movie/popular`} />
-      <Row title="評価の高い映画" url={`${TMDB_API_URL}/movie/top_rated`} />
-      <Row title="上映予定の映画" url={`${TMDB_API_URL}/movie/upcoming`} />
+      <Row title="人気の映画" data={props.popularData} />
+      <Row title="評価の高い映画" data={props.topRatedData} />
+      <Row title="近日公開予定の映画" data={props.upcomingData} />
     </Layout>
   );
 }
