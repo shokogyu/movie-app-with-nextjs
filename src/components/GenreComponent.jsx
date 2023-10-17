9216baad716a1a01379294c7510ec9ebb943177f
Shoko Okamoto <mffnoissm23@gmail.com>
2023-10-17 18:21:48 +0900

    🎨 ジャンルページ：ページ全体が読み込みとならないようにコンポーネント切り分け

---
+++ src/components/GenreComponent.jsx
@@ -0,0 +1,35 @@
+import { ModalComponent } from "@/components/ModalComponent";
+import MovieThumbComponent from "@/components/MovieThumbComponent";
+import { useModal } from "@/hooks/useModal";
+import { useMoviesByGenreId } from "@/hooks/useMoviesByGenreId";
+import { useRouter } from "next/router";
+
+export const GenreComponent = () => {
+  const router = useRouter();
+  const { data, error, isLoading } = useMoviesByGenreId();
+  const { isOpen, setIsOpen, activeMovie, handleClick } = useModal();
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>{error.message}</div>;
+  }
+
+  return (
+    <div className="p-6">
+      <h1 className="text-3xl font-bold">{router.query.name}</h1>
+      <ul className="grid grid-cols-5 mt-5 gap-2">
+        {data?.results.map((movie) => {
+          return (
+            <li key={movie.id} onClick={() => handleClick(movie)}>
+              <MovieThumbComponent movie={movie} />
+            </li>
+          );
+        })}
+      </ul>
+      <ModalComponent isOpen={isOpen} setIsOpen={setIsOpen} activeMovie={activeMovie} />
+    </div>
+  );
+};
--- src/pages/genre/[id].jsx
+++ src/pages/genre/[id].jsx
@@ -1,39 +1,10 @@
+import { GenreComponent } from "@/components/GenreComponent";
 import Layout from "@/components/Layout";
-import { ModalComponent } from "@/components/ModalComponent";
-import MovieThumbComponent from "@/components/MovieThumbComponent";
-import { useModal } from "@/hooks/useModal";
-import { useMoviesByGenreId } from "@/hooks/useMoviesByGenreId";
-import { useRouter } from "next/router";
-import React from "react";
 
 function Genre() {
-  const router = useRouter();
-  const { data, error, isLoading } = useMoviesByGenreId();
-  const { isOpen, setIsOpen, activeMovie, handleClick } = useModal();
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-
-  if (error) {
-    return <div>{error.message}</div>;
-  }
-
   return (
     <Layout>
-      <div className="p-6">
-        <h1 className="text-3xl font-bold">{router.query.name}</h1>
-        <ul className="grid grid-cols-5 mt-5 gap-2">
-          {data?.results.map((movie) => {
-            return (
-              <li key={movie.id} onClick={() => handleClick(movie)}>
-                <MovieThumbComponent movie={movie} />
-              </li>
-            );
-          })}
-        </ul>
-        <ModalComponent isOpen={isOpen} setIsOpen={setIsOpen} activeMovie={activeMovie} />
-      </div>
+      <GenreComponent />
     </Layout>
   );
 }
