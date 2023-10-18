d5bedca595e49a464dd84a3ecc6defcc77dd62a5
Shoko Okamoto <mffnoissm23@gmail.com>
2023-10-18 19:21:56 +0900

    ♻️ document.jsにあったスタイルをglobal.cssに記述

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="ja">
       <Head />
-      <body className="bg-zinc-950 text-white">
+      <body>
         <Main />
         <NextScript />
       </body>
