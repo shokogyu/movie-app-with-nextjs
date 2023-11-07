f9cf6dfc403bb7493fc688d6049a041083749c5b
Shoko Okamoto <mffnoissm23@gmail.com>
2023-11-07 18:49:14 +0900

    🩹 空白削除

--- src/components/FirstViewMovie/index.jsx
+++ src/components/FirstViewMovie/index.jsx
@@ -10,7 +10,7 @@ export const FirstViewMovie = (props) => {
       <img src={backgroundImage} alt="" className="absolute left-0 top-0" />
       <div className="absolute top-2/4 w-2/5 -translate-y-2/4">
         <h2 className="text-6xl font-black text-white drop-shadow-xl">{props.data.title}</h2>
-        <p className="mt-4 text-base text-white  drop-shadow-xl">{props.data.overview}</p>
+        <p className="mt-4 text-base text-white drop-shadow-xl">{props.data.overview}</p>
         <MyListButton movie={props.data} />
       </div>
       <div className="absolute bottom-0 left-0 h-20 w-screen bg-gradient-to-b from-transparent to-black"></div>
